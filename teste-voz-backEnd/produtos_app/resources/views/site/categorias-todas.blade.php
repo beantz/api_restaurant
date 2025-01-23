9a4fc01c1886117d9f8dc16b6d23333e6d2ac36c
Beatriz Moura <trizmours2002@gmail.com>
2025-01-22 22:28:30 -0300

    Delete teste-voz-backEnd directory

--- teste-voz-backEnd/produtos_app/resources/views/site/categorias-todas.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('site.layouts.basico')
-
-@section('conteudo')
-    @component('site._components.todos-dados', ['baseDados' => $categorias, 'dado' => 'categorias', 'msg' => $msg, 'dataQuantity' => $dataQuantity])
-    @endcomponent
-@endsection
